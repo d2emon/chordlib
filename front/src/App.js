90e48878dbe69b552afba0f1c7eaa554ae04ea67
Dmitry Kutsenko <d2emonium@gmail.com>
2018-08-25 16:48:29 +0300

    Add bootstrap

--- front/src/App.js
+++ front/src/App.js
@@ -1,21 +1,57 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    Collapse,
+    Jumbotron,
+    Button
+} from 'reactstrap';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            isOpen: false
+        };
+    }
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+    render() {
+        return (
+            <div className="App">
+                <Navbar color={"dark"} dark fixed>
+                    <NavbarBrand href="/">Аккорды</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse dark isOpen={this.state.isOpen} navbar>
+                        <h1 className="App-title"><img src={logo} className="App-logo" alt="logo" /> Welcome to React</h1>
+                    </Collapse>
+                </Navbar>
+                <Jumbotron>
+                    <h1>Welcome to React</h1>
+                    <p>
+                        To get started, edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <p>
+                        <Button
+                            bsStyle="success"
+                            bsSize="large"
+                            href="http://react-bootstrap.github.io/components.html"
+                            target="_blank">
+                            View React Bootstrap Docs
+                        </Button>
+                    </p>
+                </Jumbotron>
+            </div>
+        );
+    }
 }
 
 export default App;
