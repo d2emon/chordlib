999a93cb0b0fb779bf7bbba62adb9cab3768bd83
Dmitry Kutsenko <d2emonium@gmail.com>
2018-11-04 17:00:59 +0300

    Fetch artists from back

--- front/src/services/artist.js
+++ front/src/services/artist.js
@@ -1,41 +1,23 @@
-function Artist (name) {
-    return {
-        name,
-        slug: name,
-        description: name,
-    }
-}
-
-
-const artists = [
-    new Artist("Artists 1"),
-    new Artist("Artists 2"),
-    new Artist("Artists 3"),
-    new Artist("Artists 4"),
-    new Artist("Artists 5"),
-];
+const baseUrl = '//localhost:4000/artists';
+const letter = 'a';
 
 export default {
-    isFetched: false,
-    artists: [],
-
     fetchArtists () {
         return new Promise((resolve, reject) => {
-            if (this.isFetched) return resolve(this.artists);
-
-            this.artists = artists;
-            this.isFetched = true;
-            return resolve(this.artists);
+            fetch(`${baseUrl}/${letter}`)
+                .then(artists => resolve(artists));
         });
     },
 
     addArtist (artist) {
         return new Promise((resolve, reject) => {
+            /*
             if (!this.isFetched) this.artists = artists;
 
             this.artists.push(artist);
             this.isFetched = true;
-            return resolve(this.artists);
+            */
+            return resolve([]);
         });
     },
 }
