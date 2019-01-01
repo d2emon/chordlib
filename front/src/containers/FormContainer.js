ff90d234be312c49ea14b418b312ea7472855dbe
Dmitry Kutsenko <d2emonium@gmail.com>
2019-01-01 05:12:28 +0300

    Cleanup code

--- front/src/containers/FormContainer.js
+++ front/src/containers/FormContainer.js
@@ -5,20 +5,12 @@ import { Form } from "reactstrap";
 import AddArtistForm from '../components/AddArtistForm';
 
 class FormContainer extends Component {
-    componentWillReceiveProps(nextProps) {
-        console.log(nextProps.slug, nextProps.newSlug);
-    }
-
     render () {
-        const { handleSubmit, ...props } = this.props;
         const submitForm = (formValues) => {
             console.log('submitting Form: ', formValues);
         };
-        console.log(props);
-        if (props.slug === undefined) {
-            props.slug = props.newSlug;
-        }
-
+        const { handleSubmit, ...props } = this.props;
+        if (props.slug === undefined) props.slug = props.newSlug;
         return (
             <Form onSubmit={handleSubmit(submitForm)}>
                 <AddArtistForm
@@ -33,14 +25,12 @@ const formConfiguration = {
     form: 'artists'
 };
 
-// export default reduxForm(formConfiguration)(FormContainer);
-
-const ReduxFormContainer = reduxForm(formConfiguration)(FormContainer);
-
 const selector = formValueSelector('artists');
 
 const mapStateToProps = state => ({
     slug: selector(state, 'slug'),
 });
 
-export default connect(mapStateToProps)(ReduxFormContainer);
+FormContainer = reduxForm(formConfiguration)(FormContainer);
+
+export default connect(mapStateToProps)(FormContainer);
