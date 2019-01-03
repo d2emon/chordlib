173342754b6a525df8872af0f3595030e111ce01
Dmitry Kutsenko <d2emonium@gmail.com>
2019-01-03 03:49:06 +0300

    Update artist

--- src/models/artist.js
+++ src/models/artist.js
@@ -1,8 +1,30 @@
 import mongoose from 'mongoose';
+import fs from 'fs';
 
 const artistSchema = mongoose.Schema({
     name: String,
     slug: String,
+});
+
+artistSchema.statics.getUnprocessed = (letter) => new Promise((response, reject) => {
+    const dataFolder ='./public-data';
+    fs.readdir(dataFolder, (err, files) => {
+        const artists = files
+            .filter(file => file.startsWith(letter));
+        /*
+            .map(file => {
+                // console.log(file, file.startsWith);
+
+                console.log(file, file.startsWith(letter));
+            });
+         */
+        response(artists);
+    });
+});
+
+artistSchema.set('toJSON', {
+    virtuals: true,
+    versionKey:false,
 });
 
 export default mongoose.model('Artist', artistSchema);
