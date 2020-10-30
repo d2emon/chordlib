7c1c832c9053b9283c0f27c29b794628fd0c6eba
Dmitry Kutsenko <d2emonium@gmail.com>
2020-10-30 11:41:05 +0300

    Remove unused

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,8 +1,5 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
-import counter from '../modules/counter/reducers';
-import todos from '../modules/todos/reducers/todos';
-import visibilityFilter from '../modules/todos/reducers/visibilityFilter';
 import alphabet from '../modules/alphabet/reducers';
 import artists from './artists';
 import albums from './albums';
@@ -10,9 +7,6 @@ import songs from './songs';
 import pages from './pages';
 
 export default combineReducers({
-  counter,
-  todos,
-  visibilityFilter,
   alphabet,
   artists,
   albums,
